4061bb289ae8624e70ed2a31d0b118002ba5fe4d
Alina Laszcz <bloodypaw@gmail.com>
2023-11-05 14:11:05 +0100

    Added mapTiler logo

--- app/_components/BaseMap.tsx
+++ app/_components/BaseMap.tsx
@@ -1,28 +1,26 @@
-'use client'
+"use client";
 
-import Map, { NavigationControl } from 'react-map-gl/maplibre';
-import 'maplibre-gl/dist/maplibre-gl.css';
+import Map, { NavigationControl, useControl } from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { MaptilerLogoControl } from "@maptiler/sdk";
 
 type Props = {
-    children: React.ReactNode,
-    height?: string | number
-}
+    children: React.ReactNode;
+    height?: string | number;
+};
 
-const mapTilerMapId = '30d1bbf8-6e86-4004-96db-52701a6efe5d'
+const mapTilerMapId = "30d1bbf8-6e86-4004-96db-52701a6efe5d";
 
 // GeoPoint is [longitude, latitude], same as CAPAP
-const plCenter: GeoPoint = [19.424, 52.114]
+const plCenter: GeoPoint = [19.424, 52.114];
 
-
-// ! add MapTiler logo in left corner
 export default function BaseMap({ children, height = 400 }: Props) {
-    
     return (
         <Map
             initialViewState={{
                 longitude: plCenter[0],
                 latitude: plCenter[1],
-                zoom: 4.8
+                zoom: 4.8,
             }}
             style={{ width: "100%", height: height }}
             mapStyle={`https://api.maptiler.com/maps/${mapTilerMapId}/style.json?key=${process.env.NEXT_PUBLIC_MAPTILER_KEY}`}
@@ -32,7 +30,16 @@ export default function BaseMap({ children, height = 400 }: Props) {
             dragRotate={false}
         >
             {children}
-            <NavigationControl visualizePitch={false} showCompass={false}/>
+            <DrawControl/>
+            <NavigationControl visualizePitch={false} showCompass={false} />
         </Map>
-    )
+    );
+}
+
+function DrawControl() {
+    useControl(() => new MaptilerLogoControl(), {
+        position: "bottom-left",
+    });
+
+    return null;
 }
