76f7ff2b36511604a313f4c024f54472d3c4ea23
Weblini <bloodypaw@gmail.com>
2023-12-14 14:35:30 +0100

    removed bundle analyzer

--- next.config.js
+++ next.config.js
@@ -34,10 +34,10 @@ const nextConfig = {
     },
 }
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  })
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//     enabled: process.env.ANALYZE === 'true',
+//   })
 
-module.exports = withBundleAnalyzer(nextConfig)
+// module.exports = withBundleAnalyzer(nextConfig)
